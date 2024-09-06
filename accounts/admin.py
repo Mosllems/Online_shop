cb7a3943249175a4c2041dcbfd850676f319de21
moslem <moslemamiri82@gmail.com>
2024-09-06 08:20:54 -0700

    initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ["username", "email", "age", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("age",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', "age", ),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
