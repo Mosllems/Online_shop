554f57874e2de1aeeb96caa689ae21f63fd91c0e
moslem <moslemamiri82@gmail.com>
2024-09-08 12:34:57 +0330

    add Login and Signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,10 +6,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ("email", "username",)
+        fields = ("email", "username", "age")
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = ("email", "username",)
+        fields = ("email", "username", "age")
