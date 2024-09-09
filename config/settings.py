f8abda30a022ee720ab96d8ee1b1e9e5149f0ced
moslem <moslemamiri82@gmail.com>
2024-09-09 10:48:11 +0330

    add environments variables for debug and secret key

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+# for environments variable
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3q0wskq!@ty%7o&bn=0^97iw2t5#zyp0owbs+6wfvk=8f3)#94'
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name="home"),
-    path("about us/", views.AboutUsView.as_view(), name="aboutus"),
+    path("aboutus/", views.AboutUsView.as_view(), name="aboutus"),
 
     ]
