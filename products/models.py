e5e90a6b1eada78bcfda80c06e9afed590989a3f
moslem <moslemamiri82@gmail.com>
2024-09-17 13:47:41 +0330

    add translation

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Producer(models.Model):
@@ -30,16 +31,16 @@ class Product(models.Model):
 
 class Comment(models.Model):
     PRODUCT_STAR = {
-        "1": "Very Bad",
-        "2": "Bad",
-        "3": "Good",
-        "4": "Very Good",
-        "5": "Perfect",
+        "1": _("Very Bad"),
+        "2": _("Bad"),
+        "3": _("Good"),
+        "4": _("Very Good"),
+        "5": _("Perfect"),
     }
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="comments")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="comments")
-    text = models.TextField(verbose_name="Comment Text")
-    star = models.CharField(max_length=10, choices=PRODUCT_STAR, verbose_name="What is your score?")
+    text = models.TextField(verbose_name=_("Comment Text"))
+    star = models.CharField(max_length=10, choices=PRODUCT_STAR, verbose_name=_("What is your score?"))
     active = models.BooleanField(default=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_edited = models.DateTimeField(auto_now=True)
