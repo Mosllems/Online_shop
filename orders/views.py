9a6c12c8771766fbf4bec7e66d3a71ccd03ae62e
moslem <moslemamiri82@gmail.com>
2024-10-03 21:24:18 +0330

    add reset password file and order form

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .forms import OrderForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def order_create(request):
+    order_from = OrderForm()
+
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
     return render(request, "orders/order_create.html", context={
-        "form": OrderForm()
+        "form": order_from,
     })
